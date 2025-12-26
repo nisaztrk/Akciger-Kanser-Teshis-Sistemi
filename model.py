3bb626817b6ec81f8a1993d51bfca16e170237d8
Nisa <152283276+nisaztrk@users.noreply.github.com>
2025-12-26 14:26:03 +0300

    Add files via upload

---
+++ model.py
@@ -0,0 +1,14 @@
+import torch
+import torch.nn as nn
+from torchvision import models
+
+class ChestCancerModel(nn.Module):
+    def __init__(self, num_classes=4):
+        super(ChestCancerModel, self).__init__()
+        # Notebook'ta hangi mimariyi kullandıysan o olmalı (ResNet18)
+        self.network = models.resnet18(weights=None) 
+        in_features = self.network.fc.in_features
+        self.network.fc = nn.Linear(in_features, num_classes)
+
+    def forward(self, x):
+        return self.network(x)
